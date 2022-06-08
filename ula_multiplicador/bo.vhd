66d1de2308242c4d11badb1319ec855ee13576a8
Eduardo Perottoni <edu.perottoni@gmail.com>
2022-06-08 18:40:07 -0300

    Iniciando projeto do Trabalho Prático 1

---
+++ ula_multiplicador/bo.vhd
@@ -0,0 +1,73 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.std_logic_unsigned.all;
+
+ENTITY bo IS
+GENERIC(N: natural := 4);
+PORT (clk : IN STD_LOGIC;
+      ini, CP, CA, dec : IN STD_LOGIC;
+      entA, entB : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+      Az, Bz : OUT STD_LOGIC;
+      saida, conteudoA, conteudoB : OUT STD_LOGIC_VECTOR(3 DOWNTO 0));
+END bo;
+
+-- Sinais de comando
+-- ini = RstP = mA = CB  => ini=1 somente em S1
+-- CA=1 em S1 e em S4
+-- dec = op = m1 = m2  => dec=1 somente em S4 (estado no qual ocorre A <= A - 1 )
+-- CP=1 somente em S3 (estado no qual ocorre P <= P + B )
+
+ARCHITECTURE estrutura OF bo IS
+	
+	COMPONENT registrador_r IS
+		GENERIC(N: natural := 4);
+		PORT (clk, reset, carga : IN STD_LOGIC;
+			  d : IN STD_LOGIC_VECTOR(N-1 DOWNTO 0);
+			  q : OUT STD_LOGIC_VECTOR(N-1 DOWNTO 0));
+	END COMPONENT;
+	
+	COMPONENT registrador IS
+		PORT (clk, carga : IN STD_LOGIC;
+			  d : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+			  q : OUT STD_LOGIC_VECTOR(3 DOWNTO 0));
+	END COMPONENT;
+	
+	COMPONENT mux2para1 IS
+		GENERIC (N: natural := 4)
+		PORT ( a, b : IN STD_LOGIC_VECTOR(N-1 DOWNTO 0);
+				sel: IN STD_LOGIC;
+				y : OUT STD_LOGIC_VECTOR(N-1 DOWNTO 0));
+	END COMPONENT ;
+
+	
+	COMPONENT somadorsubtrator IS
+	PORT (a, b : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+		  op: IN STD_LOGIC;
+		  s : OUT STD_LOGIC_VECTOR(3 DOWNTO 0));
+	END COMPONENT;
+	
+    COMPONENT igualazero IS
+	PORT (a : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+          igual : OUT STD_LOGIC);
+	END COMPONENT;
+		
+	SIGNAL saimux1, saimux2, saimux3, sairegP, sairegA, sairegB, saisomasub: STD_LOGIC_VECTOR (3 DOWNTO 0);
+
+BEGIN
+	mux1: mux2para1 PORT MAP (RESULTADO_SOMA/SUB, entA, ini, );
+	regP: registrador_r
+		GENERIC MAP (N);
+		PORT MAP (clk, ini, CP, RESULTADO_SOMA/SUB);
+	regA: registrador PORT MAP ( );
+	regB: registrador PORT MAP (  );
+	mux2: mux2para1 PORT MAP (   );	
+	mux3: mux2para1 PORT MAP (   );
+	somasub: somadorsubtrator PORT MAP ();
+	geraAz: igualazero PORT MAP (  );
+	geraBz: igualazero PORT MAP (  );	
+	
+	saida <= sairegP;
+	conteudoA <= sairegA;
+	conteudoB <= sairegB;
+
+END estrutura;
---
+++ ula_multiplicador/multiplicador_v1.vhd
@@ -0,0 +1,38 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity multiplicador_v1 is 
+	generic(
+		N: natural := 4
+	);
+
+	port(
+		inicio, reset, ck: in std_logic;
+		entA, entB: in std_logic_vector(N-1 downto 0); 
+		saida: out std_logic_vector(N-1 downto 0);
+		pronto: out std_logic;
+	);
+end multiplicador_v1;
+
+architecture behavior of multiplicador_v1 is
+	signal ini, CP, CA, dec, Az, Bz: std_logic;
+	
+	component bc is
+		port (Reset, clk, inicio : IN STD_LOGIC;
+				Az, Bz : IN STD_LOGIC;
+				pronto : OUT STD_LOGIC;
+				ini, CA, dec, CP: OUT STD_LOGIC );
+	end component;
+	
+	component bo is
+		port (clk : IN STD_LOGIC;
+				ini, CP, CA, dec : IN STD_LOGIC;
+				entA, entB : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+				Az, Bz : OUT STD_LOGIC;
+				saida, conteudoA, conteudoB : OUT STD_LOGIC_VECTOR(3 DOWNTO 0));
+	end component;
+	
+begin
+	bc port map (reset, ck, inicio, Az, Bz, pronto, ini, CA, dec, CP);
+	bo port map (ck, ini, CP, CA, dec, entA, entB, Az, Bz, saida, );
+end behavior;
